ea1f59872119414cfc160516e3e76f0526734654
Roxel069 <86969414+Roxel069@users.noreply.github.com>
2023-12-18 03:00:35 +0300

    fixed ui

--- bloodbank/src/pages/admin.js
+++ bloodbank/src/pages/admin.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useMutation } from "react-query"
 import BloodRequests from "./requests.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTint } from '@fortawesome/free-solid-svg-icons';
 
 export default function admin() {
 
@@ -136,188 +138,206 @@ export default function admin() {
     }
 
 return (
-    <>
-        <div className="">
-            <h1 className=" text-center">Admin</h1>
-            <button className="primary-button" onClick={showHideAdd}>add</button>
-            <button className="primary-button" onClick={showHideRemove}>remove</button>
-            <button className="primary-button" onClick={showHideUpdate}>update</button>
-            <button className="primary-button" onClick={showHideSearch}>search history</button>
-            <button className="primary-button" onClick={showHideBloodReq}>blood req</button>
-            <button className="primary-button" onClick={showHideBloodCollection}>blood collection in given time</button>
+    <div className="flex columns-2 w-screen h-screen bg-gradient-to-l from-blue-950 from-10%">
+        <div className="flex flex-col bg-transparent shadow-lg rounded-lg ml-2 mt-2 mb-2 w-[30vh]">
+
+                <div className="mx-auto hover:text-red-600 border-2 px-4 py-2 rounded-lg mt-20 mb-8">
+                    <FontAwesomeIcon icon={faTint} size="2x" />
+                </div>
+                <button className="primary-button" onClick={showHideAdd}>
+                    add
+                </button>
+                <button className="primary-button" onClick={showHideRemove}>
+                    remove
+                </button>
+                <button className="primary-button" onClick={showHideUpdate}>
+                    update
+                </button>
+                <button className="primary-button" onClick={showHideSearch}>
+                    search history
+                </button>
+                <button className="primary-button" onClick={showHideBloodReq}>
+                    blood req
+                </button>
+                <button className="primary-button" onClick={showHideBloodCollection}>
+                    blood collection
+                </button>
+                <h1 className="text-center mt-20">Admin</h1>
         </div>
-        <div className="">
-            {
-            add &&
+        <div className="mx-auto my-auto">
             <div className="">
-                <h1>add</h1>                  
-                {addMutation.isLoading && <p>loading...</p>}
-                {! addMutation.isLoading && <div>
-                    <form onSubmit={handleAdd}>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="id">ID</label>
-                        <input type="text" placeholder="1234567890" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="fname">First Name</label>
-                        <input type="text" placeholder="ali" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="lname">Last Name</label>
-                        <input type="text" placeholder="alzhrani" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="bloodType">Blood Type</label>
-                        <input type="text" placeholder="O+" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="bd">Birth Day</label>
-                        <input type="text" placeholder="10/11/1999" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" placeholder="alo@alo.alo" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" placeholder="alialzhrani" required/>
-                    </div>
+                {
+                add &&
+                <div className="boxx">
+                    <h1 className=" text-center mb-8 font-bold">add</h1>                  
+                    {addMutation.isLoading && <p>loading...</p>}
+                    {! addMutation.isLoading && <div>
+                        <form onSubmit={handleAdd}>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="id">ID</label>
+                            <input type="text" placeholder="1234567890" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="fname">First Name</label>
+                            <input type="text" placeholder="ali" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="lname">Last Name</label>
+                            <input type="text" placeholder="alzhrani" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="bloodType">Blood Type</label>
+                            <input type="text" placeholder="O+" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="bd">Birth Day</label>
+                            <input type="text" placeholder="10/11/1999" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="email">Email</label>
+                            <input type="text" placeholder="alo@alo.alo" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" placeholder="alialzhrani" required/>
+                        </div>
+                            <div className="radio px-4 py-2 space-x-8">
+                                <label><input type="radio" name=" donor" value="true" />Donor</label>
+                                <label><input type="radio" name=" recipient" value="false"  />Recipient</label>
+                            </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <button className="primary-button" type="submit">Add User</button>
+                        </div>
+                        </form>
+                    </div> }
+                </div>
+                }
+            </div>
+            <div>
+                {
+                remove &&
+                <div className="boxx">
+                    <h1 className=" text-center mb-8 font-bold">remove</h1>
+                    {removeMutation.isLoading && <p>loading...</p>}
+                    {! removeMutation.isLoading && <div>
+                        <form onSubmit={handleRemove}>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="id">ID</label>
+                            <input type="text" placeholder="1234567890" required/>
+                        </div>
                         <div className="radio px-4 py-2 space-x-8">
                             <label><input type="radio" name=" donor" value="true" />Donor</label>
                             <label><input type="radio" name=" recipient" value="false"  />Recipient</label>
                         </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <button className="primary-button" type="submit">Add User</button>
-                    </div>
-                    </form>
-                </div> }
+                        <div>
+                            <button className="primary-button" type="submit">Remove User</button>
+                        </div>
+                        </form>
+                    </div> }
+                </div>
+                }
             </div>
-            }
-        </div>
-        <div>
-            {
-            remove &&
             <div>
-                <h1>remove</h1>
-                {removeMutation.isLoading && <p>loading...</p>}
-                {! removeMutation.isLoading && <div>
-                    <form onSubmit={handleRemove}>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="id">ID</label>
-                        <input type="text" placeholder="1234567890" required/>
-                    </div>
-                    <div className="radio px-4 py-2 space-x-8">
-                        <label><input type="radio" name=" donor" value="true" />Donor</label>
-                        <label><input type="radio" name=" recipient" value="false"  />Recipient</label>
-                    </div>
-                    <div>
-                        <button className="primary-button" type="submit">Remove User</button>
-                    </div>
-                    </form>
-                </div> }
+                {
+                update &&
+                <div className="boxx">
+                    <h1 className=" text-center mb-8 font-bold">update</h1>
+                    {updateMutation.isLoading && <p>loading...</p>}
+                    {! updateMutation.isLoading && <div>
+                        <form onSubmit={handleUpdate}>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="id">ID</label>
+                            <input type="text" placeholder="1234567890" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="fname">First Name</label>
+                            <input type="text" placeholder="ali" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="lname">Last Name</label>
+                            <input type="text" placeholder="alzhrani" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="bloodType">Blood Type</label>
+                            <input type="text" placeholder="O+" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="bd">Birth Day</label>
+                            <input type="text" placeholder="10/11/1999" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="email">Email</label>
+                            <input type="text" placeholder="alo@alo.alo" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" placeholder="alialzhrani" required/>
+                        </div>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <button className="primary-button" type="submit">Add User</button>
+                        </div>
+                        </form>
+                    </div> }
+                </div>
+                }
             </div>
-            }
-        </div>
-        <div>
-            {
-            update &&
             <div>
-                <h1>update</h1>
-                {updateMutation.isLoading && <p>loading...</p>}
-                {! updateMutation.isLoading && <div>
-                    <form onSubmit={handleUpdate}>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="id">ID</label>
-                        <input type="text" placeholder="1234567890" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="fname">First Name</label>
-                        <input type="text" placeholder="ali" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="lname">Last Name</label>
-                        <input type="text" placeholder="alzhrani" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="bloodType">Blood Type</label>
-                        <input type="text" placeholder="O+" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="bd">Birth Day</label>
-                        <input type="text" placeholder="10/11/1999" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" placeholder="alo@alo.alo" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" placeholder="alialzhrani" required/>
-                    </div>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <button className="primary-button" type="submit">Add User</button>
-                    </div>
-                    </form>
-                </div> }
+                {
+                search &&
+                <div className="boxx">
+                    <h1 className=" text-center mb-8 font-bold">search</h1>
+                    {searchMutation.isLoading && <p>loading...</p>}
+                    {! searchMutation.isLoading && <div>
+                        <form onSubmit={handleSearch}>
+                        <div className=" px-4 py-2 space-x-8 ">
+                            <label htmlFor="id">ID</label>
+                            <input type="text" placeholder="1234567890" required/>
+                        </div>
+                        <div>
+                            <button className="primary-button" type="submit">Search User</button>
+                        </div>
+                        <div>
+                            {information && <div>
+                                <p>ID - {info.person_id}</p>
+                                <p>First name - {info.first_name}</p>
+                                <p>Last name - {info.last_name}</p>
+                                <p>Blood type - {info.blood_type}</p>
+                                <p>Email - {info.email}</p>
+                                <p>Birthday - {info.b_date}</p>
+                                <p>Address - {info.address}</p>
+                                <p>Username - {info.username}</p>
+                                <p>Weight - {info.weight}</p>
+                                <p>Major disease - {info.major_disease= false? "yes":"none"}</p>
+                                <p>Donor - {info.donor_id}</p>
+                            </div>}
+                        </div>
+                        </form>
+                    </div> }
+                </div>
+                }
             </div>
-            }
-        </div>
-        <div>
-            {
-            search &&
             <div>
-                <h1>search</h1>
-                {searchMutation.isLoading && <p>loading...</p>}
-                {! searchMutation.isLoading && <div>
-                    <form onSubmit={handleSearch}>
-                    <div className=" px-4 py-2 space-x-8 ">
-                        <label htmlFor="id">ID</label>
-                        <input type="text" placeholder="1234567890" required/>
-                    </div>
-                    <div>
-                        <button className="primary-button" type="submit">Search User</button>
-                    </div>
-                    <div>
-                        {information && <div>
-                            <p>ID - {info.person_id}</p>
-                            <p>First name - {info.first_name}</p>
-                            <p>Last name - {info.last_name}</p>
-                            <p>Blood type - {info.blood_type}</p>
-                            <p>Email - {info.email}</p>
-                            <p>Birthday - {info.b_date}</p>
-                            <p>Address - {info.address}</p>
-                            <p>Username - {info.username}</p>
-                            <p>Weight - {info.weight}</p>
-                            <p>Major disease - {info.major_disease= false? "yes":"none"}</p>
-                            <p>Donor - {info.donor_id}</p>
-                        </div>}
-                    </div>
-                    </form>
-                </div> }
-            </div>
-            }
-        </div>
-        <div>
-            {
-            bloodReq &&
-            <div>
-                <h1>blood req</h1>
-                <BloodRequests />
+                {
+                bloodReq &&
+                <div className="boxx">
+                    <h1 className=" text-center mb-8 font-bold">blood req</h1>
+                    <BloodRequests />
 
+                </div>
+                }
             </div>
-            }
-        </div>
-        <div>
-            {
-            bloodCollection &&
             <div>
-                <h1>blood collection</h1>                   
-                <button className="primary-button" onClick={handleClick}> 
-                    {buttonText} 
-                </button>
+                {
+                bloodCollection &&
+                <div className="boxx">
+                    <h1 className=" text-center mb-8 font-bold">blood collection</h1>                   
+                    <button className="primary-button" onClick={handleClick}> 
+                        {buttonText} 
+                    </button>
+                </div>
+                }
             </div>
-            }
         </div>
-    </>
+    </div>
 );
 }
